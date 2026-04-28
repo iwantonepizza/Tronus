36bdd35423f1d0f03e0268f5073f7aeebd2cd42f
Паскудник <ilyamo404@gmailcom>
2026-04-28 12:20:51 +0500

    Add frontend production mocks stub

---
+++ frontend/src/build/noop-mocks-data.ts
@@ -0,0 +1,10 @@
+export const mockDecks = []
+export const mockFactions = []
+export const mockMatches = []
+export const mockModes = []
+export const mockPlayers = []
+export const mockStats = []
+export const mockCurrentUser = null
+export function getMockCommentsForMatch() {
+  return []
+}
