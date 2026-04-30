3452c19ea3f618e4f306a70466c7345ad670a441
Паскудник <ilyamo404@gmailcom>
2026-04-30 11:24:16 +0500

    v 0.1.1

---
+++ frontend/src/build/noop-mocks-data.ts
@@ -0,0 +1,13 @@
+export const mockDecks = []
+export const mockFactions = []
+export const mockMatches = []
+export const mockModes = []
+export const mockPlayers = []
+
+export function getMockCommentsForMatch() {
+  return []
+}
+
+export function getMockTimelineForMatch() {
+  return []
+}
